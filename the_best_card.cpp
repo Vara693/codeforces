96e509d8a302798864a68cda7e5accd240257b50
Vara693 <pvaradraj2@gmail.com>
2026-08-07 20:55:03 +0530

    Added daily solution 8

---
+++ the_best_card.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+
+    for (int i=2; i*i <=n; i++) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (isPrime(n+1)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
